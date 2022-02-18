4ba1dc6969fc8cd1a48f36be82dc94faed7f439b
Victor de Almeida Santana <80405995+VictorASantana@users.noreply.github.com>
2022-02-17 22:48:30 -0300

    Funções intermediárias do calculo de R prontas

--- Ex3.py
+++ Ex3.py
@@ -38,12 +38,34 @@ def calcula_vetor(vetor_coluna, i, n):
         j = j + 1
     return vetor_k
 
+#Calcula a aplicação de vetor_k nas colunas com índice maior que i
+def calcula_iteracao(vetor_coluna, vetor_k, n):
+    vetor_coluna_novo = np.zeros((n, 1))
+    j = 0
+    print(vetor_coluna)
+    print(vetor_k)
+    while j < n:
+        vetor_coluna_novo[j] = vetor_coluna[j] - 2 * (prod_escalar(vetor_coluna, vetor_k, n))/(prod_escalar(vetor_k, vetor_k, n)) * vetor_k[j]
+        j = j + 1
+    return vetor_coluna_novo
+
+#Calcula o produto escalar dos vetores A e B
+def prod_escalar(A, B, n):
+    prod = 0
+    i = 0
+    while i < n:
+        prod = prod + A[i]*B[i]
+        i = i + 1
+    return prod
+
 #Main
 matriz_A = np.array([[6, -2, -1],[-2, 6, -1], [-1, -1, 5]])
 vetor = coluna(matriz_A, 0, 3)
 print(vetor)
-print(np.linalg.norm(vetor))
 vetor_i = calcula_vetor(vetor, 0, 3)
 print(vetor_i)
+vetor = coluna(matriz_A, 1, 3)
+vetor_novo = calcula_iteracao(vetor, vetor_i, 3)
+print(vetor_novo)
 autovalores, autovetores = np.linalg.eig(matriz_A)
 autovetor_normalizado = np.zeros((3,3))
