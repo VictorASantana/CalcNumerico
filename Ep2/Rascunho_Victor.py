7da28a1260a27084fc1fbb343ec57e04f1bd4048
Victor de Almeida Santana <80405995+VictorASantana@users.noreply.github.com>
2022-03-22 15:13:27 -0300

    rascunho

--- Ep2/Rascunho_Victor.py
+++ Ep2/Rascunho_Victor.py
@@ -4,7 +4,7 @@
 #u_{N}^{j} = Ke^{L + \sigma^2\Tau_j/2}
 import numpy as np
 
-def itera(n, K, x):
+def itera(n, K, x, sigma, DeltaT, DeltaX):
     matriz_J = np.zero(shape=(2, n))
     #inicialização:
     i = 0
@@ -20,17 +20,29 @@ def itera(n, K, x):
 
 #Elementos calculados a cada iteração
 def calcula_x(DeltaX, i, L):
-    x_i = 0
-    return x_i
+    return i * DeltaX - L
+
+def calcula_tau(DeltaTau, j):
+    return DeltaTau * j
 
 def calcula_S(K, x_i, r, sigma, tau_j):
-    S = 0
+    exponencial = np.exp(x_i - (r - sigma**2/2)*tau_j)
+    S = K * exponencial
     return S
 
 def calcula_V_i_j(u_i_j, r, tau_j):
     return u_i_j * np.exp(- 1 * (r * tau_j))
 
+#Cálculo dos Deltas
+def calcula_DeltaX(L, N):
+    return 2*L/N
+
+def calcula_DeltaTau(T, M):
+    return T/M
 
 def maxi(x):
-    return x
-
+    expressao = np.exp(x) - 1
+    if(expressao >= 0):
+        return expressao
+    else:
+        return 0
